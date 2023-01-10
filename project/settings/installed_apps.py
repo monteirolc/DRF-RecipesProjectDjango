8133d565f24406ff11eb188a96be0cecd58a8966
castrolm <lucasm.castro47@gmail.com>
2023-01-10 16:47:24 -0300

    Initial commit

---
+++ project/settings/installed_apps.py
@@ -0,0 +1,14 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # Django rest framework
+    'rest_framework',
+    # A gente tem apps também
+    'recipes',
+    'authors',
+    'tag',
+]
